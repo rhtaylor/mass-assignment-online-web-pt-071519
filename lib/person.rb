0ce845cfbff3f2db515cd372068ea61928269360
Randy Taylor <randyisbusy@gmail.com>
2019-08-18 16:08:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,11 +2,11 @@ require 'pry'
 class Person
   attr_accessor :name
   def initialize(stuff)
-    dude = stuff.each do |x,y| 
+    dude = stuff.each do |x,y|
       self.send(("#{x}="), y)
       binding.pry
     end
-  
+
   end
 
 end
