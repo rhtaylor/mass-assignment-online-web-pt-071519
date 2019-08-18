3715247cdbe396a6bff5ad3c1becbe6cc5c0f73e
Randy Taylor <randyisbusy@gmail.com>
2019-08-18 16:06:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,8 @@
 require 'pry'
 class Person
-  attr_accessor :hash
-  def initialize(:hash)
-    @hash = hash
+  attr_accessor :name
+  def initialize(stuff)
+    stuff.each}{ |x,y| self.send(("#{x}="), y)}
   end
   binding.pry
 end
