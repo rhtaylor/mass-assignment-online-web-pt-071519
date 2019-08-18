fb0ce2ce3490fe01894c750d54a0ea8f6daa0a74
Randy Taylor <randyisbusy@gmail.com>
2019-08-18 16:16:15 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size,
+:wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(stuff)
+    dude = stuff.each do |x,y|
+      self.send(("#{x}="), y)
+    end
+
+  end
+
 end
